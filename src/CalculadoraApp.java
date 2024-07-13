cb229ab3d286397d9a8a451b21702bc019b7843a
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-13 16:58:44 -0500

    Refactoring: add functions and fix bug with division

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -6,44 +6,12 @@ public class CalculadoraApp {
         Scanner consola = new Scanner(System.in);
         while (true) {
             System.out.println("*** Aplicacion Calculadora ***");
-            //Menu
-            System.out.println("""
-                    1.Suma
-                    2.Resta
-                    3.Multiplica
-                    4.Division
-                    5.Salir
-                    """);
-            System.out.println("Operacion: ");
+            mostrarMenu();
             try {
                 var operacion = Integer.parseInt(consola.nextLine());
                 //Revisar si esta dentro de las opciones
                 if (operacion >= 1 && operacion <= 4) {
-                    System.out.println("Primer valor: ");
-                    var operando1 = Integer.parseInt(consola.nextLine());
-                    System.out.println("Segundo valor: ");
-                    var operando2 = Integer.parseInt(consola.nextLine());
-                    int resultado;
-                    switch (operacion) {
-                        case 1:
-                            resultado = operando1 + operando2;
-                            System.out.println("Resultado: " + resultado);
-                            break;
-                        case 2:
-                            resultado = operando1 - operando2;
-                            System.out.println("Resultado: " + resultado);
-                            break;
-                        case 3:
-                            resultado = operando1 * operando2;
-                            System.out.println("Resultado: " + resultado);
-                            break;
-                        case 4:
-                            resultado = operando1 / operando2;
-                            System.out.println("Resultado: " + resultado);
-                            break;
-                        default:
-                            System.out.println("Opcion erronea " + operacion);
-                    }
+                    ejecutarOperacion(operacion, consola);
                 } else if (operacion == 5) {
                     System.out.println("Hasta luego...");
                     break;
@@ -51,9 +19,50 @@ public class CalculadoraApp {
                     System.out.println("Opcion erronea " + operacion);
                 }
                 System.out.println();
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Ocurrio un error: " + e.getMessage());
             }
         }
+    }
+
+    private static void mostrarMenu() {
+        //Menu
+        System.out.println("""
+                1.Suma
+                2.Resta
+                3.Multiplica
+                4.Division
+                5.Salir
+                """);
+        System.out.println("Operacion: ");
+    }
+
+    private static void ejecutarOperacion(int operacion, Scanner consola) {
+        System.out.println("Primer valor: ");
+        var operando1 = Double.parseDouble(consola.nextLine());
+        System.out.println("Segundo valor: ");
+        var operando2 = Double.parseDouble(consola.nextLine());
+        double resultado;
+        switch (operacion) {
+            case 1:
+                resultado = operando1 + operando2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case 2:
+                resultado = operando1 - operando2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case 3:
+                resultado = operando1 * operando2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case 4:
+                resultado = operando1 / operando2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            default:
+                System.out.println("Opcion erronea " + operacion);
+        }
+
     }
 }
